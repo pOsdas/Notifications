0e22d0f62cc0b3b23a5ea4d489bbadd878657169
pOsdas <ratmir1099@mail.ru>
2025-11-20 02:06:26 +0300

    added pydantic config

---
+++ app/config.py
@@ -0,0 +1,66 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from django.conf import settings
+
+
+class RunModel(BaseModel):
+    host: str = "127.0.0.1"
+    port: int = 8010
+
+
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "api"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env-template", ".env"),
+        case_sensitive=False,
+    )
+    postgres_db: str
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str = "localhost"
+    postgres_port: int = 5432
+    pgdata: str
+
+    allow_db_create: int
+    db_create_retry_delay: int
+    db_create_retries: int
+
+    debug: bool = False
+    secret_key: str
+    allowed_hosts: str
+    celery_broker_url: str
+
+    twilio_from: str
+    twilio_auth_token: str
+    twilio_account_sid: str
+
+    telegram_api: str
+    bot_token: str
+
+    email_host: str
+    email_port: str
+    email_use_ssl: str
+    email_use_tls: str
+    email_host_user: str
+    email_host_password: str
+    default_from_email: str
+
+    redis_host: str
+    redis_port: int
+    redis_db: int
+
+    run: RunModel = RunModel()
+    api: ApiPrefix = ApiPrefix()
+
+
+pydantic_settings = Settings()
+# pprint(pydantic_settings.model_dump())
