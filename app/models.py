0c7f0056c5335c0d1f5760cd0e237510d0057eb1
pOsdas <ratmir1099@mail.ru>
2025-11-20 02:06:08 +0300

    fix

--- app/models.py
+++ app/models.py
@@ -1,12 +1,5 @@
-from cryptography.fernet import Fernet
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.postgres.fields import ArrayField
-from solo.models import SingletonModel
-from dotenv import load_dotenv
-import os
-
-load_dotenv(encoding="utf-8")
 
 
 class User(AbstractUser):
@@ -14,10 +7,6 @@ class User(AbstractUser):
     phone = models.CharField(max_length=20, null=True, blank=True)
     telegram_username = models.CharField(max_length=255, null=True, blank=True)
     telegram_chat_id = models.CharField(max_length=255, null=True, blank=True)
-
-    notify_email = models.BooleanField(default=True)
-    notify_sms = models.BooleanField(default=False)
-    notify_telegram = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = "Пользователь"
